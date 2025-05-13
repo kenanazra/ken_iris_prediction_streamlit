09fc0046ab10a27dd449a0b3f7f10a39c20e0895
Kenan Azra <kenanazra24@gmail.com>
2025-05-13 09:47:47 +0200

    Update streamlit-iris.py

--- streamlit-iris.py
+++ streamlit-iris.py
@@ -29,7 +29,7 @@ petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 3.8)
 petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 1.2)
 
 # Separate to X and y
-X = df.drop('Species', axis=1)
+X = df.drop(['Id', 'Species'], axis=1)
 y = df.Species
 
 # Data splitting
